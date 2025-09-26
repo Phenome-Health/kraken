ca6676b7dba92685103a848015b014cda0c5fc15
amykglen <amy.glen@phenomehealth.org>
2025-09-25 20:07:23 -0700

    Add arivale lipid extraction in different ways

--- src/mapping/extract_arivale_ids.py
+++ src/mapping/extract_arivale_ids.py
@@ -21,7 +21,16 @@ VOCAB_MAP = {
     'uniprot': 'uniprotkb',
     'gene_id': 'ensembl',
     'Labcorp LOINC ID': 'loinc',
-    'Quest LOINC ID': 'loinc'
+    'Quest LOINC ID': 'loinc',
+    'PubChem_CID': 'pubchem.compound',
+    'ChEBI_ID': 'chebi',
+    'HMDB_ID': 'hmdb',
+    'LM_ID': 'lm',
+    'KEGG_ID': 'kegg.compound',
+    'INCHI_KEY': 'inchikey',
+    'INCHIKEY': 'inchikey',
+    'RefMet_ID': 'rm',
+    'SMILES': 'smiles'
 }
 
 IDNORM = IdentifierNorm(biolink_version="4.2.5")
@@ -47,12 +56,14 @@ def get_curies(row: pd.Series, id_cols: List[str]) -> Tuple[List[str], Dict[str,
 
 
 def extract_metabolite_ids(input_dir: Path, output_dir: Path):
-    input_path = input_dir / 'metabolomics_metadata.tsv'
+    # input_path = input_dir / 'metabolomics_metadata.tsv'
+    input_path = input_dir / 'metabolites_ChemicalAnnotation-Table1.tsv'  # Latest from Lance, with refmet in it
 
-    id_cols = ['CAS', 'KEGG', 'HMDB', 'PUBCHEM']
-    other_cols = ['CHEMICAL_ID', 'SUB_PATHWAY', 'SUPER_PATHWAY', 'BIOCHEMICAL_NAME']
 
-    df = pd.read_table(input_path, dtype={'PUBCHEM': str}, usecols=other_cols + id_cols, skiprows=13)
+    id_cols = ['CAS', 'KEGG', 'HMDB', 'PUBCHEM', 'INCHIKEY', 'SMILES'] + ['PubChem_CID', 'ChEBI_ID', 'HMDB_ID', 'LM_ID', 'KEGG_ID', 'INCHI_KEY', 'RefMet_ID']
+
+    df = pd.read_table(input_path, dtype={'PUBCHEM': str})
+    df[id_cols] = df[id_cols].replace('-', np.nan)
     print(df)
 
     df[['curies', 'invalid_ids']] = df.apply(lambda row: get_curies(row, id_cols), axis=1, result_type='expand')
@@ -94,13 +105,67 @@ def extract_clinical_lab_ids(input_dir: Path, output_dir: Path):
     df.to_csv(output_path, sep='\t', index=False)
 
 
+def extract_lipid_ids(input_dir: Path, output_dir: Path):
+    input_path = input_dir / 'Arivale_lipidomics_metadata_REFMETANNOT.tsv'
+
+    id_cols = ['PubChem_CID', 'ChEBI_ID', 'HMDB_ID', 'LM_ID', 'KEGG_ID', 'INCHI_KEY', 'RefMet_ID']
+    other_cols = ['Input.name', 'Standardized.name', 'Formula', 'Exact.mass', 'Super.class', 'Main.class', 'Sub.class']
+
+    df = pd.read_table(input_path, usecols=other_cols + id_cols)
+    df[id_cols] = df[id_cols].replace('-', np.nan)
+    print(df)
+
+    df[['curies', 'invalid_ids']] = df.apply(lambda row: get_curies(row, id_cols), axis=1, result_type='expand')
+    print(df)
+
+    output_path = output_dir / 'arivale_lipids_with_curies.tsv'
+    df.to_csv(output_path, sep='\t', index=False)
+
+
+def extract_lipid_ids_norefmet(input_dir: Path, output_dir: Path):
+    input_path = input_dir / 'lipids_ChemicalAnnotation-Table1.tsv'
+
+    id_cols = ['HMDB', 'KEGG']
+    other_cols = ['CHEM_ID', 'SUPER_PATHWAY', 'SUB_PATHWAY', 'PATHWAY_SORTORDER', 'CHEMICAL_NAME', 'PLOT_NAME', 'PLATFORM']
+
+    df = pd.read_table(input_path, usecols=other_cols + id_cols)
+    df[id_cols] = df[id_cols].replace('-', np.nan)
+    print(df)
+
+    df[['curies', 'invalid_ids']] = df.apply(lambda row: get_curies(row, id_cols), axis=1, result_type='expand')
+    print(df)
+
+    output_path = output_dir / 'arivale_lipids_with_curies.tsv'
+    df.to_csv(output_path, sep='\t', index=False)
+
+
+def extract_lipid_ids_all(input_dir: Path, output_dir: Path):
+    input_path_source = input_dir / 'lipids_ChemicalAnnotation-Table1.tsv'
+    input_path_refmet = input_dir / 'Arivale_lipidomics_metadata_REFMETANNOT.tsv'
+    source_df = pd.read_table(input_path_source)
+    refmet_df = pd.read_table(input_path_refmet)
+    df = pd.merge(source_df, refmet_df, left_on='CHEMICAL_NAME', right_on='Input.name')
+
+    id_cols = ['HMDB', 'KEGG'] + ['PubChem_CID', 'ChEBI_ID', 'HMDB_ID', 'LM_ID', 'KEGG_ID', 'INCHI_KEY', 'RefMet_ID']
+
+    df[id_cols] = df[id_cols].replace('-', np.nan)
+    print(df)
+
+    df[['curies', 'invalid_ids']] = df.apply(lambda row: get_curies(row, id_cols), axis=1, result_type='expand')
+    print(df)
+
+    output_path = output_dir / 'arivale_lipids_with_curies.tsv'
+    df.to_csv(output_path, sep='\t', index=False)
+
+
 def main():
     input_dir = PROJECT_ROOT_PATH / 'input_data' / 'mapping' / 'arivale'
     output_dir = PROJECT_ROOT_PATH / 'src' / 'mapping'
 
     extract_metabolite_ids(input_dir, output_dir)
-    extract_protein_ids(input_dir, output_dir)
-    extract_clinical_lab_ids(input_dir, output_dir)
+    # extract_protein_ids(input_dir, output_dir)
+    # extract_clinical_lab_ids(input_dir, output_dir)
+    extract_lipid_ids_all(input_dir, output_dir)
 
 
 if __name__ == "__main__":
